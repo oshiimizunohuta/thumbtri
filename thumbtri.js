faea7312a1cb097e4f4e4f8e867af63115b11c85
oshiimizunohuta <g.ebodai@gmail.com>
2015-10-03 23:41:56 +0900

    読み込みファイルと同じ名前で保存

--- thumbtri.js
+++ thumbtri.js
@@ -2,20 +2,23 @@
  * thumbtri.js
  * Since 2014-07-22 16:18:22
  * @author しふたろう
- * ver 0.02
+ * ver 0.1.0
  */
 var recter = {}, trimer = {};
 var canvasWidth = 980, canvasHeight = 800;
 var thumbBoxWidth = 100, thumbBoxHeight = 100;
+var thumbThumbWidth = 100, thumbThumbHeight = 100;
+var thumbBoxDestroyMode = '';
+var thumbThumbMaked = 0;
 //TODO 選択範囲の移動
 window.onload=function()
 {
 	// var jqLayer = $('#rectLayer')
 	var jqLayer = document.getElementById('rectLayer')
 		, img = new Image()
-		, eById = function(i){return document.getElementById(i)}
-		, eByTag = function(t){return document.getElementsByTagName(t)}
-		, attr = function(i, k, v){(typeof i =="String" ? eById(i) : i).setAttribute(k,v); return i}
+		, eById = function(i){return document.getElementById(i);}
+		, eByTag = function(t){return document.getElementsByTagName(t);}
+		, attr = function(i, k, v){(typeof i =="String" ? eById(i) : i).setAttribute(k,v); return i;}
 		;
 	trimer.canvas = eById('trimBox');
 	trimer.context = trimer.canvas.getContext('2d');
@@ -40,21 +43,66 @@ window.onload=function()
 	window.onkeydown = function(e)
 	{
 		recter.ext = event.shiftKey;
-		if(recter.ext){return false}
+		// if(recter.ext){return false;}
 		return true;
 	};
 	window.onkeyup = function(e)
 	{
 		recter.ext = event.shiftKey;
-		if(recter.ext){return false}
+		// if(recter.ext){return false;}
 		return true;
 	};
-	eById('rectLayer').onmousedown = function(e)
+	
+	eById('size').onkeyup = function(e)
 	{
-		var img, pos = mousePosition(e, this)
-			, w = recter.endX - recter.startX, h = recter.endY - recter.startY
-			, panta, url
+		var s = parseInt(this.value, 10);
+		
+		if(!isNaN(s)){
+			if(s < 0){
+				s = -s;
+				thumbBoxDestroyMode = 'colorednoise';
+			}else{
+				thumbBoxDestroyMode = '';
+			}
+			 
+			thumbBoxWidth = s;
+			thumbBoxHeight = s;
+		}else{
+			s = this.value;
+			if((s + '').indexOf('*') > -1){
+				s = parseInt((s + '').replace('*', ''), 10);
+				thumbBoxDestroyMode = 'raster';
+				s = parseInt(s, 10);
+				thumbBoxWidth = s;
+				thumbBoxHeight = s;
+				// thumbThumbWidth = 20;
+				// thumbThumbHeight = 20;
+			}
+		}
+	};
+	eById('size').onkeydown = eById('size').onkeyup;
+	eById('size').focus();
+	
+	function makeImage(e, self)
+	{
+		self = self == null ? this : self;
+		var img = null, pos = mousePosition(e, self)
+			, ex = recter.endX, sx = recter.startX
+			, ey = recter.endY, sy = recter.startY
+			, w, h
+			, panta, url, mkcnt = 0, name
 			;
+		if(ex < sx){
+			recter.endX = sx;
+			recter.startX = ex;
+		}
+		if(ey < sy){
+			recter.endY = sy;
+			recter.startY = ey;
+		}
+		w = recter.endX - recter.startX;
+		h = recter.endY - recter.startY;
+		
 		recter.mouseDown = true;
 			
 		if(pos.x < recter.endX && pos.x > recter.startX && pos.y < recter.endY && pos.y > recter.startY){
@@ -73,22 +121,40 @@ window.onload=function()
 				attr(attr(recter.canvas, 'width', thumbBoxWidth + 'px'), 'height', thumbBoxHeight + 'px');
 				recter.context.putImageData(img, 0,0);
 			}
+			
+			if(thumbBoxDestroyMode == 'colorednoise'){
+				recter.context.putImageData(coloredNoise(recter.context.getImageData(0, 0, thumbBoxWidth, thumbBoxHeight)), 0, 0);
+			}else if(thumbBoxDestroyMode == 'raster'){
+				// applyRaster(recter, createRaster(thumbBoxWidth, thumbBoxHeight, 0.2, 91 * Math.PI / (30), thumbThumbMaked));
+				applyRaster(recter, createRaster(thumbBoxWidth, thumbBoxHeight, 0.1, 62 * Math.PI / (30), thumbThumbMaked));
+			}
 
 			//srcを取得してimgタグに貼る
 			panta = eById('boxin');
 			url = recter.ext ? recter.canvas.toDataURL('image/jpeg') : recter.canvas.toDataURL();
-			panta.innerHTML += '<a href="' + url + '" target="_blank"><img src="' + url + '" height="' + thumbBoxHeight + '" /></a>';
+			name = recter.name;
+			
+//			panta.innerHTML += '<a href="' + url + '" target="_blank"><img src="' + url + '" height="' + thumbThumbWidth + '" /></a>';
+			panta.innerHTML += '<img src="' + url + '" height="' + thumbThumbWidth + '" />';
+			panta.onclick = function(){
+				screenshot(url, name);
+			};
 			
 			recter.canvas.setAttribute('width', canvasWidth + 'px'); recter.canvas.setAttribute('height', canvasHeight + 'px');
+			if(thumbThumbMaked++ < 30 && thumbBoxDestroyMode == 'raster'){
+				makeImage(e, self);
+			}
 		}
 		recter.startX = pos.x;
 		recter.startY = pos.y;
 		
 	};
+	eById('rectLayer').onmousedown = makeImage;
+
 	recter.canvas.onmouseup = function(e)
 	{
 		recter.mouseDown = false;
-		var w = Math.abs(recter.endX - recter.startX), h = Math.abs(recter.endY - recter.startY)
+		var w = Math.abs(recter.endX - recter.startX), h = Math.abs(recter.endY - recter.startY);
 		
 	};
 	recter.canvas.onmousemove = function(e)
@@ -110,6 +176,40 @@ window.onload=function()
 		
 	};
 	
+	function coloredNoise(imgData)
+	{
+		var i = 0, w = imgData.width, h = imgData.height, d = imgData.data
+			, len = w * h * 4
+		;
+		while(i < len){
+			d[i++] = (Math.random() * 255) | 0;
+			d[i++] = (Math.random() * 255) | 0;
+			d[i++] = (Math.random() * 255) | 0;
+			d[i++] = 255;
+		}
+		return imgData;
+	}
+	
+	function createRaster(w, h, rate, rad, cycle)
+	{
+		var raster = {width: w, height: h, rate: rate, rad: rad, cycle: cycle};
+		return raster;
+	}
+	
+	function applyRaster(scroll, raster)
+	{
+		var i = 0, w = raster.width, h = raster.height
+			, len = h, img = new Image(w, h)
+		;
+		
+		img.src = scroll.canvas.toDataURL();
+		scroll.context.clearRect(0, 0, w, h);
+		while(i < h){
+			scroll.context.drawImage(img, 0, i, w, 1, w * raster.rate * Math.sin((i + raster.cycle) * raster.rad), i, w, 1);
+			i++;
+		}
+
+	}
 	
 	function dragoverFile(e)
 	{
@@ -119,20 +219,24 @@ window.onload=function()
 	{
 		var i, data = e.dataTransfer
 			, reader = new FileReader()
-			, arrowTypes = ['image/png', 'image/jpeg', 'image/gif'];
+			, arrowTypes = ['image/png', 'image/jpeg', 'image/gif']
+			, filedata
+			;
 		e.preventDefault();
 		for(i = 0; i < data.files.length; i++){
-			if(arrowTypes.indexOf(data.files[i].type) == -1){
+			filedata = data.files[i];
+			if(arrowTypes.indexOf(filedata.type) == -1){
 				return;
 			}
-			reader.readAsDataURL(data.files[i]);
+			reader.readAsDataURL(filedata);
+			recter.name = filedata.name;
 		}
 		reader.onload = function(e){
 			var img = new Image();
 			img.src = e.target.result;
 			img.onload = function(){
 				loadCanvas(img);
-			}
+			};
 		};
 	}
 
@@ -155,7 +259,7 @@ function loadCanvas(img)
 
 	if(drawHeight > cHeight){
 		drawHeight = cHeight;
-		drawWidth = (iWidth * heightRate) | 0
+		drawWidth = (iWidth * heightRate) | 0;
 	}	
 	drawX = ((cWidth - drawWidth) / 2) | 0;
 	drawY = ((cHeight - drawHeight) / 2) | 0;
@@ -167,7 +271,7 @@ function loadCanvas(img)
 function bgFill(w, h)
 {
 	var 
-	i, j, bg1 = 'rgb(96, 96, 96)', bg2 = 'rgb(128, 128, 128)', text = 'rgb(224, 224, 248)'
+	i, j, bg1 = 'rgb(96, 96, 96)', bg2 = 'rgb(128, 128, 128)', text = 'rgb(224, 224, 248)';
 
 	for(j = 0; j < h; j += 20){
 		for(i = 0; i < w; i += 20){
@@ -183,7 +287,7 @@ function bgFill(w, h)
 }
 function mousePosition(event, e)
 {
-	var mdown = recter.mouseDown
+	var mdown = recter.mouseDown;
 		 canvasPos = e.getBoundingClientRect();
 		 
 	return{
@@ -191,3 +295,48 @@ function mousePosition(event, e)
 		y:event.clientY - canvasPos.top
 	};
 }
+
+			
+function screenshot(dataurl, name)
+{
+	var base64 = dataurl
+		, blob = Base64toBlob(base64)
+	;
+	saveBlob(blob, name);
+}
+
+
+function saveBlob(blobdat, filedat) {
+	if (/*@cc_on ! @*/false) {// IEの場合
+		window.navigator.msSaveBlob(blobdat, filedat);
+	} else {
+		var url = (window.URL || window.webkitURL)
+			, data = url.createObjectURL(blobdat)
+			, e = document.createEvent("MouseEvents")
+			, a = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+		;
+		e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		a.href = data;
+		a.download = filedat;
+		a.dispatchEvent(e);
+	}
+}
+
+
+
+function Base64toBlob(base64dat) {
+	var i
+		, tmp = base64dat.split(',')
+		, data = atob(tmp[1])
+		, mime = tmp[0].split(':')[1].split(';')[0]
+		, arr = new Uint8Array(data.length)
+	;
+	for ( i = 0; i < data.length; i++) {
+		arr[i] = data.charCodeAt(i);
+	}
+	return new Blob([arr], {
+		type : mime
+	});
+}
+
+
